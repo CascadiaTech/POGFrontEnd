878d8b064ca9df4a46ed058d927b02db84d4b335
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2023-09-23 13:38:34 -0700

    staking

--- components/Stake/LinqStakeModal.tsx
+++ components/Stake/LinqStakeModal.tsx
@@ -1,13 +1,9 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { useAccount, useContractRead, useContractWrite } from "wagmi";
-import "react-toastify/dist/ReactToastify.css";
-import fourteenDayStackAbi from "../../contracts/abi/14DayStackabi.json";
-import error from "next/error";
 import TabMenu from "./LinqStakeTabMenu";
 import LinqStakeTabMenu from "./LinqStakeTabMenu";
-const fourteenDayContractAddress = "0x7A8D1608327EdBdD5C4f1367fD6dD031F21AD7eb";
-const LPtokenContract = "0xA8A837E2bf0c37fEf5C495951a0DFc33aaEAD57A";
+
 
 interface LinqStakeModalProps {
   isOpen: boolean;
