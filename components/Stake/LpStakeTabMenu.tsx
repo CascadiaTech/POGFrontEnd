a4785c83081e18052cc71edeff089a28567e4ff9
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2023-09-23 17:03:46 -0700

    ffS

--- components/Stake/LpStakeTabMenu.tsx
+++ components/Stake/LpStakeTabMenu.tsx
@@ -29,8 +29,8 @@ export default function LpStakeTabMenu({
   setToken,
 }: LpStakeTabMenuProps) {
   const { address } = useAccount();
-  const StaqeFarm = "0x0AE06016e600f65393072e06BBFCDE07266adD0d";
-  //const StaqeFarm = "0x03b20d5C096b694607A74eC92F940Bc91bDEb5d5";
+  //const StaqeFarm = "0x0AE06016e600f65393072e06BBFCDE07266adD0d";
+  const StaqeFarm = "0x03b20d5C096b694607A74eC92F940Bc91bDEb5d5";
   let current_chain = 5;
   const LPtokenContract = "0xbD08FcFd3b2a7bB90196F056dea448841FC5A580";
   const linqContract = "0x5f35753d26C5dDF25950c47E1726c2e9705a87EA";
@@ -231,6 +231,8 @@ export default function LpStakeTabMenu({
     args: [address],
     onSuccess(data: any) {
       setUserDetails(data);
+      setUnlockTime(Number(data[2].toString()));
+
     },
   });
 
@@ -262,9 +264,8 @@ export default function LpStakeTabMenu({
       SetShowPerpOptions(true);
     }
 
-    if (userdetails) {
-      setUnlockTime(userdetails[2]);
-    }
+    console.log(unlocktime)
+
   }, [address]);
 
   return (
