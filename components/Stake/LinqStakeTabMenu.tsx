359a1a29f10bd22262dc5fbf6cdf20b7deca65c3
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2023-09-25 15:50:57 -0700

    edits@

--- components/Stake/LinqStakeTabMenu.tsx
+++ components/Stake/LinqStakeTabMenu.tsx
@@ -311,74 +311,27 @@ export default function LinqStakeTabMenu({
         <h2 className="text-lg text-white">
           Please enter the amount of tokens
         </h2>
-
-        <input
-          type="number"
-          id="stakeInput"
-          className="w-64 border my-2 border-gray-300 outline-none p-2 pr-10 text-black"
-          style={{ fontFamily: "ethnocentricRg" }}
-          onChange={(e) => {
-            const value = parseFloat(e.target.value); // Parse the input value as a float
-            if (!isNaN(value) && value >= 1) {
-              set_amountLinQ(value); // Set the parsed value to the state
-            }
-            set_amountLinQ(Number(e.target.value));
-          }}
-        />
-        {Allowance >= _amountLinQ ? (
-          <>
-            {" "}
-            {staqeLoad ? (
-              <Spin size="large" indicator={antIcon} className="add-spinner" />
-            ) : (
-              <button
-                style={{ fontFamily: "GroupeMedium" }}
-                className="font-sans w-64 text-center cursor-pointer text-md rounded-lg text-center focus:ring-2 focus:ring-blue-500 bg-black border-white border-2 text-white bg-black py-2 "
-                type="button"
-                onClick={() => HandleStaQe()}
-              >
-                Stake
-              </button>
-            )}
-          </>
-        ) : (
-          <>
-            {" "}
-            {approveLoad ? (
-              <Spin size="large" indicator={antIcon} className="add-spinner" />
-            ) : (
-              <button
-                style={{ fontFamily: "GroupeMedium" }}
-                className="font-sans w-64 cursor-pointer text-md rounded-lg text-center border-white border-2 text-white bg-black py-2 px-4 sm:px-5 md:px-5"
-                type="button"
-                onClick={() => Approve()}
-              >
-                Approve
-              </button>
-            )}
-          </>
-        )}
-
-        <div className="flex-row justify-center my-3 items-center">
-          {unstaqeLoad ? (
-            <Spin size="large" indicator={antIcon} className="add-spinner" />
-          ) : (
-            <button
-              disabled={userdetails ? userdetails[0] < _amountLinQ : true}
-              onClick={() => HandleUnStaQe()}
-              style={{ fontFamily: "GroupeMedium" }}
-              className="font-sans cursor-pointer w-64 text-md rounded-lg text-center focus:ring-2 focus:ring-blue-500 border-white border-2 text-white bg-black py-2 px-4 sm:px-5 md:px-5"
-              type="button"
-            >
-              UnStake
-            </button>
-          )}
-        </div>
-        <div className="flex flex-col justify-center items-center my-3">
-          {Number(unlocktime?.toString()) != 0 &&
-          Number(unlocktime?.toString()) < currentTime &&
-          owned == false &&
-          GAllowance >= Number(userdetails[0].toString()) / 10 ** 18 ? (
+        <div className="flex flex-col items-center justify-center">
+          <input
+            type="number"
+            id="stakeInput"
+            className="w-64 border my-2 border-gray-300 outline-none p-2 pr-10 text-black"
+            style={{ fontFamily: "ethnocentricRg" }}
+            value={_amountLinQ}
+            onChange={(e) => {
+              //   let value = e.target.valueAsNumber; // Get the input value as a number
+              let inputValue = Number(e.target.value); // Parse the input value as a number
+              if (inputValue < 0) {
+                // If the input is negative, reset it to a positive value or display an error message
+                set_amountLinQ(0); // You can choose to set it to 0 or any other default value
+                // Alternatively, you can display an error message to the user
+                // console.error('Please enter a positive number');
+              } else {
+                set_amountLinQ(inputValue);
+              }
+            }}
+          />
+          {Allowance >= _amountLinQ ? (
             <>
               {" "}
               {staqeLoad ? (
