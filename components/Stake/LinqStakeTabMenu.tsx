cb077bad5a906e72a7b15ac5997094156d63242e
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2023-09-27 10:18:54 -0700

    blockQ

--- components/Stake/LinqStakeTabMenu.tsx
+++ components/Stake/LinqStakeTabMenu.tsx
@@ -68,9 +68,10 @@ export default function LinqStakeTabMenu({
   const { data } = useBlockNumber({
     chainId: current_chain,
     watch: true,
-    onSuccess(data: any) {
-      setCurrentTime(data);
+    onBlock(blockNumber) {
+      setCurrentTime(blockNumber);
     },
+
   });
 
   /*
@@ -366,7 +367,7 @@ export default function LinqStakeTabMenu({
     >
       <div>
         <h1 className="text-xl md:text-2xl mb-12 text-white">
-          Linq token StaQing
+          Linq Token StaQing
         </h1>
         <h2 className="text-lg text-white">
           Please enter the amount of tokens
@@ -403,7 +404,7 @@ export default function LinqStakeTabMenu({
                   type="button"
                   onClick={() => HandleStaQe()}
                 >
-                  Stake
+                  StaQe
                 </button>
               )}
             </>
@@ -475,7 +476,7 @@ export default function LinqStakeTabMenu({
                           className="font-sans cursor-pointer w-64 text-md rounded-lg text-center focus:ring-2 focus:ring-blue-500 border-white border-2 text-white bg-black py-2 px-4 sm:px-5 md:px-5"
                           type="button"
                         >
-                          UnStake
+                          UnStaQe
                         </button>
                       </>
                     )}
