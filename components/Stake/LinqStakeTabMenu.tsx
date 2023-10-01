3210a10ca841811705d1a26fee5f345646cbe7c8
CascadiaTech <107577095+CascadiaTech@users.noreply.github.com>
2023-10-01 11:13:37 -0700

    Update nostake for linqstake

--- components/Stake/LinqStakeTabMenu.tsx
+++ components/Stake/LinqStakeTabMenu.tsx
@@ -532,7 +532,14 @@ export default function LinqStakeTabMenu({
                           disabled={
                             userdetails ? userdetails[0] < _amountLinQ : true
                           }
-                          onClick={() => HandleUnStaQe()}
+                              onClick={() =>   
+                     Swal.fire({
+                    icon: "warning",
+                    title: "Warning",
+                    text: "We are currently working on the Unstake UI. It will be fixed and active shortly, please be patient for the next hour or two.",
+                    showCancelButton: true, // Show Cancel button
+                    confirmButtonText: "Ok", // Change the Confirm button text
+                  })}
                           style={{ fontFamily: "GroupeMedium" }}
                           className="font-sans cursor-pointer w-64 text-md rounded-lg text-center focus:ring-2 focus:ring-blue-500 border-white border-2 text-white bg-black py-2 px-4 sm:px-5 md:px-5"
                           type="button"
