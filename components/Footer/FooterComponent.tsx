d62ca342bfeda60f33ad2f193258f338cde5ab35
Conner Holloway <connerholloway@Conners-MacBook-Pro-2.local>
2024-07-12 09:46:07 -0700

    a-tag

--- components/Footer/FooterComponent.tsx
+++ components/Footer/FooterComponent.tsx
@@ -19,41 +19,6 @@ export default function FooterComponent() {
   return (
     <div>
       <footer className="bg-gray-200 p-5 w-full h-full">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0"></div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Resources
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400">
-                <li className="mb-4">
-                  <a href="https://linktr.ee/enterthesyndicate" className="hover:underline">
-                    Link Tree 
-                    <br/>
-                    
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-            </div>
-            <div>
-              <ul className="text-gray-600 dark:text-gray-400">
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <a href="/" className="hover:underline">
-              The Syndicate
-            </a>
-            . All Rights Reserved.
-          </span>
-        </div>
       </footer>
     </div>
   );
